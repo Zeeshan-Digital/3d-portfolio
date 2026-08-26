c1b225d314a4c5efbb8997cf9154dbb1c23c3982
Zeeshan Waseem <zeeshanin2020@gmail.com>
2026-08-26 14:33:11 +0500

    TechStack.tsx

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,14 +13,14 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/photoshop.png",
+  "/illustrator.png",
+  "/canva.png",
+  "/wordpress.png",
+  "/seo.png",
+  "/contentwriting.png",
+  "/socialmedia.png",
+  "/powerpoint.png",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
@@ -168,7 +168,7 @@ const TechStack = () => {
 
   return (
     <div className="techstack">
-      <h2> My Techstack</h2>
+      <h2> My Skills</h2>
 
       <Canvas
         shadows
